6d678bdae11c314f9266e4ef8661320c11660773
邪七 <2152860@qq.com>
2018-06-15 17:34:24 +0800

    '添加测试s'

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 // export * from './specifications.api';
-// 20180615-17:20
+// 20180615-17:20    
 export * from './src/localstorage.service';
 export * from './src/service.pinyin';
 export * from './src/text.pinyin';
