02af77915967849e2638450ca3f95c9e7fcbd130
金飞 <28206254@qq.com>
2018-06-15 11:52:37 +0800

    s

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+/// <reference path="./src/service.LocalStorage.ts" />
+/// <reference path="./src/service.pinyin.ts" />
+
 // export * from './specifications.api';
+export * from './src/service.LocalStorage';
 export * from './src/service.pinyin';
 export * from './src/text.pinyin';
 export * from './src/baseClass';
