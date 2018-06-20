adf8a011c9ff035e413fe0f48cd0b5eb03e68a0e
金飞 <28206254@qq.com>
2018-06-20 19:42:31 +0800

    s

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 
 // export * from './specifications.api';
 // 20180615-17:20    
-export * from './src/localstorage.service';
+export * from './src/localStorage.service';
 export * from './src/service.pinyin';
 export * from './src/text.pinyin';
 export * from './src/baseClass';
--- src/localstorage.service.ts
+++ src/localstorage.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class LocalStorage {
-  
+export class LocalStorageService {
+
   public localStorage: any;
 
   constructor() {
